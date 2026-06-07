c1e1136b6a6c810e9d381782ff426c349da74d5b
Kaif <kaifkalanthar@gmail.com>
2026-06-07 23:37:36 +0530

    Refactor layout components for improved structure and add account creation prompts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Rubik } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const rubik = Rubik({
+  variable: '--font-rubik',
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
@@ -23,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html lang="en" className={`${rubik.variable} h-full antialiased`} style={{ fontFamily: 'var(--font-rubik)' }}>
       <body className="min-h-full flex flex-col">
         <div>{children}</div>
       </body>
--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,19 +1,31 @@
 import Image from 'next/image';
+import { TextStyle } from '@/components/typography';
 
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex items-center justify-center h-screen relative">
+    <div className="h-screen relative">
       <Image src="background.svg" alt="Background" fill className="inset-0 object-cover -z-10" />
-      <div className="w-[50%] h-[90vh] flex items-center justify-center relative">
-        <Image
-          src="login-layout-illustrator.svg"
-          className="absolute bottom-0 right-0"
-          alt="Login Background"
-          width={600}
-          height={380}
-        />
+      <div className="p-12 max-w-354 mx-auto flex items-center justify-center h-screen relative">
+        <div className="w-[50%] h-[90vh] relative">
+          <header className="flex flex-col gap-2 mb-8">
+            <TextStyle variant="sm-regular">Let&apos;s get started</TextStyle>
+            <TextStyle variant="2xl-bold">Create your account</TextStyle>
+            <TextStyle variant="md-regular" subdued>
+              Follow the steps to create your account
+            </TextStyle>
+          </header>
+          <Image
+            src="login-layout-illustrator.svg"
+            className="absolute bottom-0 left-0"
+            alt="Login Background"
+            width={600}
+            height={380}
+          />
+        </div>
+        <div className="w-[50%] h-[90vh] bg-white rounded-2xl p-8 shadow-[-16px_4px_35px_rgba(0,0,0,0.03)]">
+          {children}
+        </div>
       </div>
-      <div className="w-[50%]">{children}</div>
     </div>
   );
 }
